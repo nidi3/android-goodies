7e2f39e814266b588aa4cba6c29d3049d8ca59ef
Stefan Niederhauser <ghuder5@gmx.ch>
2015-02-06 13:02:48 +0100

    add ignore to FragmentHolder

--- src/main/java/guru/nidi/android/layout/FragmentHolder.java
+++ src/main/java/guru/nidi/android/layout/FragmentHolder.java
@@ -6,45 +6,56 @@ import android.support.v4.app.FragmentActivity;
 
 import java.lang.reflect.Field;
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 
 /**
  *
  */
 public class FragmentHolder {
-    private static final Map<Class<?>, List<Field>> fragmentFields = new HashMap<>();
+    private final List<Field> allFields = new ArrayList<>();
+    private final List<Field> fields = new ArrayList<>();
     private final FragmentActivity activity;
 
     public FragmentHolder(FragmentActivity activity) {
         this.activity = activity;
-        List<Field> fields = getFields();
         for (Field f : getClass().getDeclaredFields()) {
             if (Fragment.class.isAssignableFrom(f.getType())) {
                 f.setAccessible(true);
+                allFields.add(f);
+            }
+        }
+        fields.addAll(allFields);
+    }
+
+    public void ignore(String... ignores) {
+        fields.clear();
+        final List<String> ignoreList = Arrays.asList(ignores);
+        for (Field f : allFields) {
+            if (!ignoreList.contains(f.getName())) {
                 fields.add(f);
             }
         }
     }
 
-    private List<Field> getFields() {
-        List<Field> fields = fragmentFields.get(getClass());
-        if (fields == null) {
-            fields = new ArrayList<>();
-            fragmentFields.put(getClass(), fields);
+    public List<Fragment> getFragments(Fragment... fragments) {
+        final List<Fragment> res = new ArrayList<>();
+        for (int i = 0; i < fragments.length; i++) {
+            if (hasFragment(fragments[i])) {
+                res.add(fragments[i]);
+            }
         }
-        return fields;
+        return res;
     }
 
     public void init(Bundle bundle) {
         try {
             if (bundle == null) {
-                for (Field f : getFields()) {
+                for (Field f : fields) {
                     f.set(this, f.getType().newInstance());
                 }
             } else {
-                for (Field f : getFields()) {
+                for (Field f : fields) {
                     f.set(this, activity.getSupportFragmentManager().getFragment(bundle, f.getName()));
                 }
             }
@@ -57,13 +68,27 @@ public class FragmentHolder {
 
     public void save(Bundle bundle) {
         try {
-            for (Field f : getFields()) {
+            for (Field f : fields) {
                 activity.getSupportFragmentManager().putFragment(bundle, f.getName(), (Fragment) f.get(this));
             }
         } catch (IllegalAccessException e) {
             throw new RuntimeException(e);
         } catch (IllegalStateException e) {
-            e.printStackTrace();
+            throw new RuntimeException(e);
         }
     }
+
+    private boolean hasFragment(Fragment fragment) {
+        try {
+            for (Field f : fields) {
+                if (f.get(this) == fragment) {
+                    return true;
+                }
+            }
+            return false;
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
--- src/main/java/guru/nidi/android/view/SimplePagerAdapter.java
+++ src/main/java/guru/nidi/android/view/SimplePagerAdapter.java
@@ -4,39 +4,41 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  *
  */
 public class SimplePagerAdapter extends FragmentPagerAdapter {
-    private final Fragment[] fragments;
+    private final List<Fragment> fragments;
 
-    public SimplePagerAdapter(FragmentManager fm, Fragment... fragments) {
+    public SimplePagerAdapter(FragmentManager fm, List<Fragment> fragments) {
         super(fm);
         this.fragments = fragments;
     }
 
+    public SimplePagerAdapter(FragmentManager fm, Fragment... fragments) {
+        this(fm, Arrays.asList(fragments));
+    }
+
     @Override
     public CharSequence getPageTitle(int position) {
-        final Fragment fragment = fragments[position];
+        final Fragment fragment = fragments.get(position);
         return fragment instanceof Named ? ((Named) fragment).getName() : "";
     }
 
     @Override
     public Fragment getItem(int i) {
-        return fragments[i];
+        return fragments.get(i);
     }
 
     @Override
     public int getCount() {
-        return fragments.length;
+        return fragments.size();
     }
 
     public int indexOf(Fragment fragment) {
-        for (int i = 0; i < fragments.length; i++) {
-            if (fragments[i] == fragment) {
-                return i;
-            }
-        }
-        return -1;
+        return fragments.indexOf(fragment);
     }
 }
